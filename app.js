1726bdb40e2f139b6f8359f0f01d75e565591c4c
yunyun9229 <chuyun.huang625@gmail.com>
2022-02-23 18:49:16 -0600

    project intial set up and complete simple HTML portion

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+   const grid = document.querySelector(".grid");
+   let squares = Array.from(document.querySelectorAll(".grid div"));
+   const scoreDisplay = document.getElementById("score");
+   const startButton = document.getElementById("start-button");
+
+   console.log(squares);
+});
